2a0c0cd1fc41d4e6ceed54b881826f1aaf4b1f7d
DanVi <danicrisa@hotmail.com>
2022-06-07 00:38:29 -0300

    typescript

---
+++ app.js
@@ -0,0 +1,12 @@
+function somar(num1, num2) {
+    return num1 + num2;
+}
+console.log(somar(1, 2));
+function dividir(num1, num2) {
+    return num1 / num2;
+}
+console.log(dividir(1, 2));
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+var resultado2 = multiplicar(2, 3);
---
+++ app.ts
@@ -0,0 +1,18 @@
+function somar(num1:number, num2:number) {
+  return num1+num2
+}
+
+console.log(somar(1,2))
+
+function dividir(num1:number, num2:number): number{
+  return num1/num2
+}
+
+console.log(dividir(1,2))
+
+function multiplicar(num1, num2) {
+  return num1*num2
+}
+
+const resultado2 = multiplicar(2,3) as number
+
